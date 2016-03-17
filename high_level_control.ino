6310b98bee02349e1f909377932398682d8a11d0
brianpage <bpage@mtu.edu>
2016-03-17 10:05:33 -0400

    Added files via upload
    Functional breakout of ROUGHIE control to make it easier to update.
    
    No comments yet, hasn't been tested either.

---
+++ high_level_control.ino
@@ -0,0 +1,83 @@
+int sawtooth(int &lin,int &rot,int &pump,int &linMode) {
+  int pumpDone;
+  if(currentState == DOWNGLIDE) {
+    pump = param.tankBackLimit;
+    pumpDone = checkPump(pump);//Returns true if done pumping
+    //Linear Calculations
+    if(linPID || linFuzzy) {
+      linMode = PWM;
+      lin = param.linNoseDownTarget;
+    }
+    if(feedforward && (!pumpDone || feedforward) && (!linPID && !linFuzzy)) {
+      linMode = POSITION;
+      lin = param.downFeedforward;
+    }
+
+    //Roll Calculations
+    if(circle || (dubin && (millis() - t0 < param.dubinTime))) {
+      rot = param.rollover;
+    } else {
+      rot = 0.0;
+    }
+
+    if(millis() - t0 > param.desTime) {
+      currentState = NEUTRAL;
+      nextState = UPGLIDE;
+      t0 = millis();
+    }
+  }
+
+  if(currentState == NEUTRAL) {
+    pump = param.tankMid;
+    pumpDone = checkPump(pump);//Returns true if done pumping
+
+    if(millis() - t0 > param.neutralTime) {
+      currentState = nextState;
+      t0 = millis();
+    }
+  }
+
+  if(currentState == UPGLIDE) {
+    pump = param.tankFrontLimit;    
+    pumpDone = checkPump(pump);//Returns true if done pumping
+    //Linear Calculations
+    if(linPID || linFuzzy) {
+      linMode = PWM;
+      lin = param.linNoseUpTarget;
+    }
+    if(feedforward && (!pumpDone || feedforward) && (!linPID && !linFuzzy)) {
+      linMode = POSITION;
+      lin = param.upFeedforward;
+    }
+
+    //Roll Calculations
+    if(circle) {
+      rot = -param.rollover;
+    } else if(dubin && (millis() - t0 < param.dubinTime)) {
+      rot = param.rollover;
+    } else {
+      rot = 0.0;
+    }
+
+        
+    if(millis() - t0 > param.riseTime) {
+      currentState = NEUTRAL;
+      nextState = DOWNGLIDE;
+      t0 = millis();
+    }
+  }
+
+  if(linMode == PWM) {
+    float error_prev, error_act;
+    if(linPID) {
+      lin = linPIDRate(lin);
+    }
+    if(linFuzzy) {
+      lin = linFuzzyRate(lin);
+    }
+  }
+
+  if(turnFeedback) {
+    rot = rotPID(rot);
+  }
+}
---
+++ low_level_control.ino
@@ -0,0 +1,31 @@
+float rotPID(float rot) {
+  float error = -(rot - imu.roll);
+  rollI = rollI + error/100.0;
+  rotOutput = param.rollkp*error + param.rollki+rollI + rotOutput;
+  rotOutput = constrain(rotOutput,-45.0,45.0);
+  return rotOutput;  
+}
+
+static float error_prev, error_act;
+int linPIDRate(int lin) {
+  float P,D;
+  error_prev = error_act;
+  error_act = lin - imu.pitch;
+  P = error_act;
+  linI = linI + error_act;
+  D = error_act - error_prev;
+
+  return param.linkp*P + param.linki*linI + param.linkd*D;
+  
+}
+
+int linFuzzyRate(int lin) {
+  float error_act = abs(lin - imu.pitch);
+  fuzzy->setInput(1,error_act);
+  fuzzy->fuzzify();
+  int rate = constrain(abs(fuzzy->defuzzify(1)),0,255);
+  if (error_act < 0) {
+    rate = -rate;
+  }
+  return rate;  
+}
