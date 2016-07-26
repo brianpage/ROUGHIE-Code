d6f42279de2ea709a522c71e46c7101c14b51c16
brianpage <bpage@mtu.edu>
2016-07-26 14:14:54 -0400

    Add files via upload

---
+++ fastROUGHIE/07-26/glider_functions.ino
@@ -0,0 +1,116 @@
+int getFiltAnalog(int APIN)//Averaging filter to get rid of some noise, may or may not be needed
+{
+  int val = 0;
+  for(int a=0; a<10; a++) {
+    val = val + analogRead(APIN);
+  }
+  val = val/10;
+  return val;
+}
+
+void updateGlider() {
+  glider.linPos = getFiltAnalog(linPos);
+  glider.tankPos = getFiltAnalog(tankLevel);
+  glider.pressure = getFiltAnalog(pressureSensorPin);
+  glider.runTime = millis() - tstart;
+  //Serial.println(glider.runTime);
+  
+}
+
+bool checkPump(int tank) {//boolean check to see if the pump is at the target
+  if(abs(glider.tankPos - tank) < 10) {
+    //Serial.println("Pump Done2");
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void checkFF(int tank, int angle, bool &flag) {
+  if(abs(glider.tankPos - tank) < 10 && abs(imu.pitch - angle) < param.FFerror) {
+    //Serial.println("FF Complete");
+    flag = true;
+  }
+  
+  if(millis() - t0 > param.FFtime) {
+    //Serial.println("FF TIMEOUT");
+    flag = true;
+  }  
+}
+
+bool checkMass(int mass) {//boolean check to see if the mass is at the target
+  if(abs(glider.linPos - mass) < 10) {
+    //Serial.println("Mass done2");
+    return true;
+  } else {
+    return false;
+  }
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void turnOff() {// turn off pump and motor
+  digitalWrite(motAPWM, 0);//Turn off linear motor
+  digitalWrite(motStdby, LOW);//Turn off motor controller
+  digitalWrite(pumpOn, LOW);//Turn off pump
+  //Serial.println(F("Turning Off"));
+}
+
+void updateIMU(void) {//Update the IMU structure
+  // Read IMU
+  boolean inChar = Serial3.find('$');
+  String input = Serial3.readStringUntil('$');
+  float temp = input.substring(39,47).toFloat() + imu.pitchOffset;
+  imu.pitchD  = imu.pitch - temp;
+  imu.pitch = temp;
+  temp = input.substring(30,38).toFloat();
+  imu.yawD = imu.yaw - temp;
+  imu.yaw = temp;
+  temp = input.substring(48,56).toFloat() + imu.rollOffset;
+  imu.rollD = imu.roll - temp;
+  imu.roll = temp;
+}
+
+void updateCompass(void) {//Update compass measurements. Works with HMC5883L chip from Sparkfun
+  Wire.beginTransmission(address);
+  Wire.write(0x03);
+  Wire.endTransmission();
+  Wire.requestFrom(address,6);
+  if(6<=Wire.available()) {
+    compass.rawX = Wire.read()<<8;
+    compass.rawX |= Wire.read(); //X lsb
+    compass.rawZ = Wire.read()<<8; //Z msb
+    compass.rawZ |= Wire.read(); //Z lsb
+    compass.rawY = Wire.read()<<8; //Y msb
+    compass.rawY |= Wire.read(); //Y lsb
+  }
+  //Perform tilt compensation based on IMU measurements, works reasonably well up to about 45 degrees.
+  //float compX = compass.rawX*cos(0.017444*imu.pitch)+compass.rawY*sin(0.017444*imu.roll)*sin(0.017444*imu.pitch)+compass.rawZ*cos(0.017444*imu.roll)*sin(0.017444*imu.pitch);
+  //float compY = compass.rawY*cos(0.017444*imu.roll)-compass.rawZ*sin(0.017444*imu.roll);
+//  Serial.print(compass.rawX);
+//  Serial.print("\t");
+//  Serial.print(compass.rawY);
+//  Serial.print("\t");
+//  Serial.print(compass.rawZ);
+//  Serial.print("\t");
+//  Serial.print(rotStor);
+  //float compX = compass.rawX*cos(0.01744*imu.pitch)+compass.rawZ*sin(0.01744*imu.pitch);
+  //float compY = compass.rawX*sin(0.01744*imu.roll)*sin(0.01744*imu.pitch)+compass.rawY*cos(0.01744*imu.roll)-compass.rawZ*sin(0.01744*imu.roll)*cos(0.01744*imu.pitch);
+  float cosAngle = cos((imu.roll+rotStor)*0.01744);
+  float sinAngle = sin((imu.roll+rotStor)*0.01744);
+  float Bfy = compass.rawY*cosAngle-compass.rawZ*sinAngle;
+  float Bz = compass.rawY*sinAngle+compass.rawZ*cosAngle;
+  cosAngle = cos(imu.pitch*0.01744);
+  sinAngle = sin(imu.pitch*0.01744);
+  float Bfx = compass.rawX*cosAngle+Bz*sinAngle;
+  float Bfz = -compass.rawX*sinAngle+Bz*cosAngle;
+  compass.heading = 180*atan2(-Bfy,Bfx)/3.14;
+  //compass.heading = 180*atan2(-compY,compX)/3.14;//-4-4/60;//heading adjusted for declination in Houghton
+  //Serial.println(compass.heading);
+//  Serial.print("\t");
+//  Serial.println(compass.heading);
+}
+
---
+++ fastROUGHIE/07-26/high_level_control.ino
@@ -0,0 +1,174 @@
+int sawtooth(int &lin,float &rot,int &pump,int &linMode) {
+  //sawtooth calculates the required glide targets based on system parameters and timing
+  
+  int pumpDone;
+  int glideCompleted = 0;//returns 0 unless it just completed a glide
+  if(currentState == DOWNGLIDE) {//DOWNGLIDE tries to send the ROUGHIE into a downward glide
+    
+    pump = param.tankBackLimit;//pump to the backlimit
+    //pumpDone = checkPump(pump);//Returns true if done pumping
+    checkFF(pump,param.linNoseDownTarget,flag);
+    
+    //Linear Calculations
+    if((feedforward && !flag) || (!linPID && !linFuzzy)) {//Feedforward/feedback logic to determine the required output
+      linMode = POSITION;
+      lin = param.downFeedforward;
+    } else {
+      linMode = PWM;
+      lin = param.linNoseDownTarget;
+    }
+
+    //Roll Calculations
+//    if(circle || (dubin && (millis() - t0 < param.dubinTime))) {
+//      rot = param.rollover;
+//    } else {
+//      rot = 0.0;
+//    }
+
+    if(!delayRoll) {
+      if(circle || (dubin && (millis() - t0 < param.dubinTime))) {
+        rot = param.rollover;
+      } else {
+        rot = 0.0;
+      }
+    } else {
+      if((flag) && (circle || (dubin && (millis() - t0 > param.dubinTime)))) {//Feedforward/feedback logic to determine the required output
+        rot = param.rollover;
+      } else {
+        rot = 0.0;
+      }
+    }
+
+    if(millis() - t0 > param.desTime - 1000) {
+      downLoops = downLoops + 1;
+      lastDownAngle = lastDownAngle + imu.pitch;
+      Serial.println(lastDownAngle/downLoops);
+    }
+    
+    if(millis() - t0 > param.desTime) {//Once its been gliding for desTime go to the next state
+      currentState = NEUTRAL;
+      nextState = UPGLIDE;
+      rotStorage = rot;;
+      t0 = millis();
+      //delay(50);
+      Serial.println(F("DOWN GLIDE DONE"));
+      
+    }
+  }
+
+  if(currentState == NEUTRAL) {
+    pump = param.tankMid;//Send the pump to the center
+    if(feedforward && millis() - t0 > pumpTime) {
+      //Serial.println(nextState);
+      if(nextState == UPGLIDE) {
+        lin = param.upFeedforward;
+      }
+      if(nextState == DOWNGLIDE) {
+        lin = param.downFeedforward;
+      }
+      mode = POSITION;
+    } else {
+      lin = 0;
+      mode = PWM;
+    }
+    rot = rotStorage;
+
+    
+    if(millis() - t0 > param.neutralTime) {//Once neutral time is complete go to the next state
+      currentState = nextState;
+      t0 = millis();
+      Serial.println(F("Neutral Done"));
+    }
+    flag = false;
+  }
+
+  if(currentState == UPGLIDE) {
+    
+    if(millis() - t0 > param.riseTime - 1000) {
+      upLoops = upLoops + 1;
+      lastUpAngle = lastUpAngle + imu.pitch;
+      Serial.println(lastUpAngle/upLoops);
+    }
+    
+    pump = param.tankFrontLimit;  //Empty the ballast tank  
+    //pumpDone = checkPump(pump);//Returns true if done pumping
+    checkFF(pump,param.linNoseUpTarget,flag);
+    
+    
+    //Linear Calculations
+    if((feedforward && !flag) || (!linPID && !linFuzzy)) {//feedforward/feedback calculations
+      linMode = POSITION;
+      lin = param.upFeedforward;
+    } else {
+      linMode = PWM;
+      lin = param.linNoseUpTarget;
+    }
+
+    //Roll Calculations
+    if(circle) {
+      rot = -param.rollover;
+    } else if(dubin && (millis() - t0 < param.dubinTime)) {
+      rot = param.rollover;
+    } else {
+      rot = 0.0;
+    }
+
+    if(!delayRoll) {
+      if(circle) {
+        rot = -param.rollover;
+      } else if(dubin && (millis() - t0 < param.dubinTime)) {
+        rot = param.rollover;
+      } else {
+        rot = 0.0;
+      }
+    } else {
+      if((flag)) {//Feedforward/feedback logic to determine the required output
+        if(circle) {
+          rot = -param.rollover;
+        } else if(dubin && (millis() - t0 < param.dubinTime)) {
+          rot = param.rollover;
+        } else {
+          rot = 0.0;
+        }
+      } else {
+        rot = 0.0;
+      }
+    }
+    
+
+
+    if(millis() - t0 > param.riseTime) {
+      currentState = NEUTRAL;
+      nextState = DOWNGLIDE;
+      glideCompleted = 1;
+      rotStorage = rot;
+      t0 = millis();
+      Serial.println(F("Glide Completed"));
+    }
+  }
+
+  if(linMode == PWM) {//If its PWM mode either do fuzzy or PID
+    float error_prev, error_act;
+    if(linPID) {
+      lin = linPIDRate(lin);
+    }
+    if(linFuzzy) {
+      lin = linFuzzyRate(lin);
+    }
+  } else {
+    linI = 0.0;
+  }
+
+  if(turnFeedback) {
+    rot = rotPID(rot);
+  }
+  if(glideCompleted && !(completedGlides+1 >= param.number_of_glides)) {
+    lastUpAngle = 0.0;
+    lastDownAngle = 0.0;
+    upLoops = 0;
+    downLoops = 0;
+  }
+  
+  return glideCompleted;
+  
+}
